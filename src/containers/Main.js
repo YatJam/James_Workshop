965f5152d44694a1ef14f563fc0f4ac125b07b76
james yates <yates_7@hotmail.com>
2023-08-15 16:37:23 +0100

    Fixed Routes
    Removed uselocation and transition fades due to preventing page change on nav. Fixed now.

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -6,32 +6,14 @@ import PortfolioMain from './PortfolioMain'
 import About from '../components/About'
 
 const Main = () =>{
-
-  const location = useLocation();
-    const [displayLocation, setDisplayLocation] = useState(location);
-    const [transitionStage, setTransitionStage] = useState('fadeIn');
-
-  useEffect(() => {
-    if (location !== displayLocation) setTransitionStage('fadeOut');
-    }, [location, displayLocation]);
-
-
+    
     return (
       <>
-       <section
-            className={`${transitionStage}`}
-            onAnimationEnd={() => {
-                if (transitionStage === 'fadeOut') {
-                    setTransitionStage('fadeIn');
-                    setDisplayLocation(location);
-                }
-            }}>
-        <Routes location={displayLocation}>
+        <Routes>
             <Route path='/James_Workshop' element={<PortfolioMain />} />
             <Route path='/about' element={<About /> } />
             <Route path='/' element={<PortfolioMain />} />
         </Routes>
-        </section>
       </>
     );
   }
